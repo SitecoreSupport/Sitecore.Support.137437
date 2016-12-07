962dcee457b6c423becd6723da9d70c8cd5c584d
DK\AMCH <amch@sitecore.net>
2016-12-07 07:36:20 +0200

    Commit 8.2 up1

---
+++ Sitecore.Support.137437/Sitecore.Support.137437/IndexDatabasePropertyStore.cs
@@ -0,0 +1,119 @@
+﻿using Sitecore.Abstractions;
+using Sitecore.Collections;
+using Sitecore.Configuration;
+using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.Maintenance;
+using Sitecore.Data;
+using Sitecore.Diagnostics;
+using System;
+using System.Runtime.CompilerServices;
+
+namespace Sitecore.Support.ContentSearch.Maintenance
+{
+    public class IndexDatabasePropertyStore : IIndexPropertyStore
+    {
+        private SafeDictionary<string, string> defaultValues;
+        private readonly IEvent events;
+        private readonly IFactory factory;
+        private Sitecore.Data.Database innerStore;
+
+        public IndexDatabasePropertyStore() : this(ContentSearchManager.Locator.GetInstance<IEvent>(), ContentSearchManager.Locator.GetInstance<IFactory>())
+        {
+        }
+
+        internal IndexDatabasePropertyStore(IEvent events, IFactory factory)
+        {
+            this.defaultValues = new SafeDictionary<string, string>();
+            this.events = events;
+            this.factory = factory;
+            this.SuppressPropertyChangedEvents = true;
+        }
+
+        public void Add(string key, string value)
+        {
+            string str = this.MasterKey + "_" + key;
+            string str2 = this.InnerStore.Properties[str];
+            using (new PropertyChangedEventDisabler(this.SuppressPropertyChangedEvents))
+            {
+                this.InnerStore.Properties[str] = string.IsNullOrEmpty(str2) ? value : (str2 + "," + value);
+            }
+            object[] parameters = new object[] { str, value };
+            (this.events ?? ContentSearchManager.Locator.GetInstance<IEvent>()).RaiseEvent("indexing:propertyadd", parameters);
+        }
+
+        public void Clear(string prefix)
+        {
+            using (new PropertyChangedEventDisabler(this.SuppressPropertyChangedEvents))
+            {
+                this.InnerStore.Properties.RemovePrefix(this.MasterKey + "_" + prefix);
+            }
+        }
+
+        public void ClearAll()
+        {
+            using (new PropertyChangedEventDisabler(this.SuppressPropertyChangedEvents))
+            {
+                this.InnerStore.Properties.RemovePrefix(this.MasterKey);
+            }
+        }
+
+        public string Get(string key)
+        {
+            string str2;
+            string str = this.MasterKey + "_" + key;
+            if (this.defaultValues.ContainsKey(str))
+            {
+                str2 = this.defaultValues[str];
+            }
+            else
+            {
+                str2 = this.InnerStore.Properties[str];
+                if (string.IsNullOrEmpty(str2))
+                {
+                    this.defaultValues[str] = str2;
+                }
+            }
+            object[] parameters = new object[] { str, str2 };
+            //(this.events ?? ContentSearchManager.Locator.GetInstance<IEvent>()).RaiseEvent("indexing:propertyget", parameters);
+            return str2;
+        }
+
+        public void Set(string key, string value)
+        {
+            string str = this.MasterKey + "_" + key;
+            using (new PropertyChangedEventDisabler(this.SuppressPropertyChangedEvents))
+            {
+                this.InnerStore.Properties[str] = value;
+                if (this.defaultValues.ContainsKey(str))
+                {
+                    this.defaultValues.Remove(str);
+                }
+            }
+            object[] parameters = new object[] { str, value };
+            //(this.events ?? ContentSearchManager.Locator.GetInstance<IEvent>()).RaiseEvent("indexing:propertyset", parameters);
+        }
+
+        public string Database { get; set; }
+
+        protected Sitecore.Data.Database InnerStore
+        {
+            get
+            {
+                if (this.innerStore == null)
+                {
+                    Assert.IsNotNull(this.Database, "Database is not set");
+                    Assert.IsTrue((this.factory ?? ContentSearchManager.Locator.GetInstance<IFactory>()).GetDatabase(this.Database) != null, "Cannot find the inner store database");
+                    this.innerStore = (this.factory ?? ContentSearchManager.Locator.GetInstance<IFactory>()).GetDatabase(this.Database);
+                }
+                return this.innerStore;
+            }
+        }
+
+        public string Key { get; set; }
+
+        public string MasterKey =>
+            $"{this.Key}_{Settings.InstanceName}";
+
+        public bool SuppressPropertyChangedEvents { get; set; }
+    }
+}
---
+++ Sitecore.Support.137437/Sitecore.Support.137437/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Sitecore.Support.137437")]
+[assembly: AssemblyDescription("Patch for Sitecore XP 8.2 rev. 161115 (8.2 Update-1)")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Sitecore.Support.137437")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("905ae185-53b8-4190-bbf1-67a1b53ecc02")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
